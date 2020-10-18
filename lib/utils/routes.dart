defd841b8541f30f80d7de6703c87f7f52011b13
Cameron Johnston <camj.dev@gmail.com>
2020-10-17 19:09:35 -0700

    add utils dir with routes & constants

---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  // Add app wide routes here that are then used in main or the widget itself
+  // Could even have the map within here
+
+  // Alternatively, add a static 'route' string on each widget and build up here or in main
+}
